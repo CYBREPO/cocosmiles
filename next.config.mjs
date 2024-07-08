4d7ccec4a323d0aa1c733d71ef01b264ea21ccfb
chaitanyashirodkar010 <chaitanyashirodkar010@gmail.com>
2024-07-08 16:15:34 +0530

    /cocosmiles

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // experimental: {
-  //   appDir: true,
-  // },
-  // compiler: {
-  //   styledComponents: true,
-  // },
-  
-};
-
-export default nextConfig;
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   basePath: "/cocosmiles",
-// output: "export",
-// reactStrictMode: true,
+//   // experimental: {
+//   //   appDir: true,
+//   // },
+//   // compiler: {
+//   //   styledComponents: true,
+//   // },
+  
 // };
+
+// export default nextConfig;
+
+ /** @type {import('next').NextConfig} */
+ const nextConfig = {
+   basePath: "/cocosmiles",
+ output: "export",
+ reactStrictMode: true,
+ };
 
 // export default nextConfig;
